625c63d2f2a8e7e20f6e6ed50a25264dd20db907
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-09 21:41:11 +0900

    Update layout and form components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='ja'>
+      <body className={inter.className}>
+        <main className='flex justify-center items-center h-screen bg-gray-100'>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
